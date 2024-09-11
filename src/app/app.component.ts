372b8c805f0a510827589a289ec70772818c83e4
iFederx <federico.brignone@gmail.com>
2024-09-11 15:07:18 +0200

    bug present

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
+import { animate, keyframes, query, stagger, style, transition, trigger } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
-@Component({
+@Component( {
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    CommonModule,
+    MatTabsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-})
+  styleUrl: './app.component.scss',
+  animations: [
+    trigger( 'fadeInStagger', [
+      transition( '* => *', [
+        query( '.fade-in', [
+          style( { opacity: 0 } ),
+          stagger( '1000ms', [
+            animate( 2000, keyframes( [
+              style( { opacity: 0, transform: 'translateY(-50px)', offset: 0 } ),
+              style( { opacity: .5, transform: 'translateY(15px)', offset: 0.3 } ),
+              style( { opacity: 1, transform: 'translateY(0)', offset: 1.0 } ),
+            ] )
+            ),
+          ] ),
+        ],
+          { optional: true } )
+      ] )
+    ] ),
+  ]
+} )
 export class AppComponent {
-  title = 'issueTabs_v18';
+  // #region Properties (1)
+
+  public title = 'issueTabs_v18';
+
+  // #endregion Properties (1)
 }
